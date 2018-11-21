2e356b3572dc954ad11f8a4603d6ead9a4c36f2c
wangwei1996 <1849553578@qq.com>
2018-11-21 16:36:11 +0800

    DSA2 all

---
+++ DataStructs/UnionFind/UnionFindV4/main.c
@@ -0,0 +1,17 @@
+//
+// Created by wei on 11/3/18.
+//
+
+#include <stdio.h>
+#include "UnionFind.h"
+
+
+int main(int argc, char **argv) {
+
+    for (int i = 0; i < 20; ++i) {
+        printf("Hello World\n");
+        fprintf(stderr, "Hello Wang Wei\n");
+    }
+
+    return 0;
+}
