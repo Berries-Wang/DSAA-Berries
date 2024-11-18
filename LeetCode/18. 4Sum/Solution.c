c11a0e3b99fe9e5fe0cf5c3bfab2b65269a47d8f
Wei.Wang <ziyang.ww@raycloud.com>
2024-11-18 19:24:19 +0800

    18. 4Sum

---
+++ LeetCode/18. 4Sum/Solution.c
@@ -0,0 +1,18 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * 动规五部曲
+ * 1. 确定dp数组
+ * 2. 确定递推公式
+ * 3. 初始化dp数组
+ * 4. 确定遍历顺序
+ * 5. 遍历/打印dp数组
+ */
+int **fourSum(int *nums, int numsSize, int target, int *returnSize,
+              int **returnColumnSizes) {
+  return NULL;
+}
+
+int main(int argc, char **argv) { return 0; }
